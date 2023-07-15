96a4ab4667f86c626db2ccf8b57683105b236288
Darzhi Mantusov <darzhi@mail.ru>
2023-07-15 18:19:10 +0300

    Another type of using class methods in jinja2 templates

--- ex1.py
+++ ex1.py
@@ -1,16 +1,24 @@
 from jinja2 import Template
 
+
 class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+
+    def getName(self):
+        return self.name
+
+    def getAge(self):
+        return self.age
+
 
 n = 'Федор'
 a = 28
 
 per = Person(n, a)
 
-tm = Template('Мне {{ p.age * 2 }} лет и зовут {{ p.name }}')
-msg = tm.render(p = per)
+tm = Template('Мне {{ p.getAge() }} лет и зовут {{ p.getName() }}')
+msg = tm.render(p=per)
 
 print(msg)
