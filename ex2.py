050802821a1f64e6b66962dce2b3ab68a77a79c9
Darzhi Mantusov <darzhi@mail.ru>
2023-07-15 21:52:01 +0300

    Lesson 2. Цикл for, {% for item in iter %} что-делать, {% endfor %}

--- ex2.py
+++ ex2.py
@@ -1,9 +1,18 @@
 from jinja2 import Template
 
-link = """В HTML-документе ссылки определяются так:
-<a href='#'>Ссылка</a>"""
+cities = [{'id': 1, 'city': 'Москва'},
+          {'id': 5, 'city': 'Тверь'},
+          {'id': 7, 'city': 'Минск'},
+          {'id': 8, 'city': 'Смоленск'},
+          {'id': 11, 'city': 'Калуга'}]
 
-tm = Template("{{ link | e }}")
-msg = tm.render(link=link)
+link = '''<select name="cities">
+{% for c in cities %}
+    <option value="{{c['id']}}">{{c['city']}}</option>
+{% endfor %}
+<select>'''
+
+tm = Template(link)
+msg = tm.render(cities = cities)
 
 print(msg)
