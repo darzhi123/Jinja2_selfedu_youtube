b0673365f67aa83a6f468c694cbd2b49978c2abb
Darzhi Mantusov <darzhi@mail.ru>
2023-07-17 15:38:45 +0300

    Lesson 3.

---
+++ ex3.py
@@ -0,0 +1,14 @@
+from jinja2 import Template
+
+cars = [
+    {'model': 'Ауди', 'price': 23_000},
+    {'model': 'Шкода', 'price': 17_300},
+    {'model': 'Вольво', 'price': 44_300},
+    {'model': 'Фольксваген', 'price': 21_300}
+]
+
+tpl = "Суммарная цена автомобилей {{ cs | sum(attribute='price') }}"
+tm = Template(tpl)
+msg = tm.render(cs = cars)
+
+print(msg)
